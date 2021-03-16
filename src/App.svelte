14a4805690de00ea8066e416d4a49a623ba4f7fa
NagiC <huoshengchiou@gmail.com>
2021-03-17 00:44:09 +0800

    '[eventhandle]'

--- src/App.svelte
+++ src/App.svelte
@@ -4,6 +4,11 @@
   const handleClick = () => {
     color = "orange";
   };
+
+  const handleInput = (e) => {
+    //單向改變值，只要color被任何地方使用，所有的地方都會跟著變動
+    color = e.target.value;
+  };
 </script>
 
 <!--  component 沒有export-->
@@ -11,12 +16,18 @@
 <!-- root conponent -->
 <main>
   <h1>Hello {name}!</h1>
-  <h1>{color}</h1>
+  <h1 style="color:{color}">{color}</h1>
   <p>
     Visit the <a href="https://svelte.dev/tutorial">Hello kotake</a> to learn how
     to build Svelte apps.
   </p>
   <button on:click={handleClick}>update</button>
+  <!-- 只需要單向綁定 -->
+  <input type="text" on:input={handleInput} />
+  <!-- 雙向綁定值 -->
+  <input type="text" on:input={handleInput} value={color} />
+  <!-- 和上面行為相同 雙向綁定值-->
+  <input type="text" bind:value={color} />
 </main>
 
 <style>
