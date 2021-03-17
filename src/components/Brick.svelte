a90ca5ccf93cb2c846b49ce3d02f5fe279acbdc8
NagiC <huoshengchiou@gmail.com>
2021-03-18 00:10:49 +0800

    '[eventForword]'

---
+++ src/components/Brick.svelte
@@ -0,0 +1,18 @@
+<script>
+  export let move = true;
+</script>
+
+<!-- 這裡不定義onclick的行為，Event Forwarding會把操作權移給上方的parent -->
+<div class="brick" class:move on:click />
+
+<style>
+  .brick {
+    width: 100px;
+    height: 100px;
+    background: blue;
+    transition: 0.75s;
+  }
+  .move {
+    transform: translateX(100px);
+  }
+</style>
--- src/components/Login.svelte
+++ src/components/Login.svelte
@@ -1,7 +1,9 @@
 <script>
-
+  //利用export來取得外面相同名稱的value，如果上一層不存在一樣變數走default
+  export let key = "default value";
+  export let value;
+  export let passColor = "red";
 </script>
 
-<div>
-    Hello
-</div>
+<div>Hello {key}</div>
+<div>color{passColor}</div>
